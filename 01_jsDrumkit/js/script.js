b7c3452aaf60a21f1d3109b755e0cabcf2d32357
Marius Kroh <mail@mariuskroh.de>
2019-11-26 13:17:35 +0100

    added click functionality + minor css changes

--- 01_jsDrumkit/js/script.js
+++ 01_jsDrumkit/js/script.js
@@ -3,16 +3,31 @@ function removeTransition(event) {
   this.classList.remove("playing"); // this --> left of the dot - keys
 }
 
-function playSound(event) {
+function playKey(event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
   if (!audio) return; // stop the function
   audio.currentTime = 0; // rewind
   audio.play();
-  key.classList.add("playing");
+  key.classList.add("playing"); // add class
+
 }
+
+function playClick(event) {
+    const clicked = event.target;
+    const pad = event.target.classList[0];
+    const sound = document.querySelector(`audio[class="${pad}"]`);
+    if (!pad) return;
+    sound.currentTime = 0;
+    sound.play();
+    clicked.classList.add("playing"); // add class
+  }
 
 const keys = document.querySelectorAll(".key");
 keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playKey);
 
-window.addEventListener("keydown", playSound);
+//onclick
+keys.forEach(key => key.addEventListener("click", playClick));
+
+
