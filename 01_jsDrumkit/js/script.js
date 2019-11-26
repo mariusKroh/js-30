1abf6aa8021a22297139126d44fe65e75b1ba597
Marius Kroh <mail@mariuskroh.de>
2019-11-26 16:18:50 +0100

    add display functionality (basic)

--- 01_jsDrumkit/js/script.js
+++ 01_jsDrumkit/js/script.js
@@ -1,33 +1,38 @@
+// set display variable
+const display = document.getElementById("display");
+// remove playing class after transition
 function removeTransition(event) {
-  if (event.propertyName !== "transform") return; //only listen to transform
-  this.classList.remove("playing"); // this --> left of the dot - keys
+    if (event.propertyName !== "transform") return; //only listen to transform
+    this.classList.remove("playing"); // this --> left of the dot - keys
 }
-
+// play sound when pressing key + set display
 function playKey(event) {
-  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-  if (!audio) return; // stop the function
-  audio.currentTime = 0; // rewind
-  audio.play();
-  key.classList.add("playing"); // add class
-
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    const name = key.classList[2];
+    if (!audio) return; // stop the function
+    audio.currentTime = 0; // rewind
+    audio.play();
+    key.classList.add("playing"); // add class
+    display.innerHTML = name;
 }
-
+// play sound onclick + set display
 function playClick(event) {
     const clicked = event.target;
     const pad = event.target.classList[0];
     const sound = document.querySelector(`audio[class="${pad}"]`);
+    const name = event.target.classList[2];
     if (!pad) return;
     sound.currentTime = 0;
     sound.play();
     clicked.classList.add("playing"); // add class
-  }
+    display.innerHTML = name;
+}
 
+// shoot event listeners + functions
 const keys = document.querySelectorAll(".key");
 keys.forEach(key => key.addEventListener("transitionend", removeTransition));
 window.addEventListener("keydown", playKey);
 
-//onclick
+
 keys.forEach(key => key.addEventListener("click", playClick));
-
-
