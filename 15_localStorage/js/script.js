ac59a8dacd4595fd661fcaea36be95407ed5f742
Marius Kroh <mail@mariuskroh.de>
2019-12-09 13:54:42 +0100

    basic functionality

--- 15_localStorage/js/script.js
+++ 15_localStorage/js/script.js
@@ -1,6 +1,6 @@
   const addItems = document.querySelector('.add-items');
   const itemsList = document.querySelector('.plates');
-  const items = [];
+  const items = JSON.parse(localStorage.getItem("items")) || [];
 
   function addItem(event) {
       event.preventDefault();
@@ -12,6 +12,7 @@
 
       items.push(item);
       populateList(items, itemsList);
+      localStorage.setItem("items", JSON.stringify(items))
       this.reset();
   }
 
@@ -19,15 +20,23 @@
       platesList.innerHTML = plates.map((plate, i) => {
           return `
           <li>
-          <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? "checked" : ""}/>
-          <label for="item ${i}">${plate.text}</label>
-          </li>
-          
-          `
+            <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+            <label for="item${i}">${plate.text}</label>
+          </li>`
       }).join("");
 
   }
 
-
+  function toggleDone(event) {
+      if (!event.target.matches("input")) return; // skip this unless it is an input 
+      const el = event.target;
+      const index = el.dataset.index;
+      items[index].done = !items[index].done;
+      localStorage.setItem("items", JSON.stringify(items))
+      populateList(items, itemsList);
+  }
 
   addItems.addEventListener("submit", addItem);
+  itemsList.addEventListener("click", toggleDone);
+
+  populateList(items, itemsList);
