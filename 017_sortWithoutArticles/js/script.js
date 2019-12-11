929f7615bdb2ea080e66f46b48b51e2ccd30569f
Marius Kroh <mail@mariuskroh.de>
2019-12-11 10:17:47 +0100

    basic functionality

--- 017_sortWithoutArticles/js/script.js
+++ 017_sortWithoutArticles/js/script.js
@@ -13,3 +13,12 @@
       "Anywhere But Here",
       "An Old Dog"
   ];
+
+  const list = document.querySelector("#bands");
+
+  function strip(bandName) {
+      return bandName.replace(/^(a |the |an )/i, "").trim();
+  }
+  const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1)
+
+  list.innerHTML = sortedBands.map(band => `<li>${band}</li>`).join("");
